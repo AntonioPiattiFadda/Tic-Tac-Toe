41e3c021e111b9a9836b23e255f98ea5fffe2a9d
Antonio Piatti Fadda <antonio.piattifadda@gmail.com>
2023-05-10 20:04:27 -0300

    confetti and restart button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Board from './components/Board/Board';
 import ScoreBoard from './components/ScoreBoard/ScoreBoard';
 import Turn from './components/Turnero/Turn';
+import confetti from 'canvas-confetti';
 
 const winningPositions = [
   [0, 1, 2],
@@ -15,80 +16,90 @@ const winningPositions = [
   [2, 4, 6],
 ];
 
-
 const App = () => {
-
-  const [ turn, setTurn] = useState('X');
-  const [ squares, setSquares ] = useState(Array(9).fill(null));
-  const [ winningSquares, setWinningSquares] = useState([]);
-  const [ score, setScore ] = useState({
-    X : 0,
-    O : 0
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winningSquares, setWinningSquares] = useState([]);
+  const [score, setScore] = useState({
+    X: 0,
+    O: 0,
   });
 
-  const checkForWinner = newSquares => {
-    for(let i = 0; i < winningPositions.length; i++) {
-      const [a,b,c] = winningPositions[i];
-      if(newSquares[a] && newSquares[a] === newSquares[b] && newSquares[a] === newSquares[c]) {
+  const checkForWinner = (newSquares) => {
+    for (let i = 0; i < winningPositions.length; i++) {
+      const [a, b, c] = winningPositions[i];
+      if (
+        newSquares[a] &&
+        newSquares[a] === newSquares[b] &&
+        newSquares[a] === newSquares[c]
+      ) {
         endGame(newSquares[a], winningPositions[i]);
-        
-        return
+
+        return;
       }
     }
 
-    if(!newSquares.includes(null)) {
-      endGame(null, Array.from(Array(10).keys()));
-      return
+    if (!newSquares.includes(null)) {
+      endGame(null, Array.from(Array(9).keys()));
+      return;
     }
     setTurn(turn === 'X' ? 'O' : 'X');
-  }
-    
+  };
+
   const endGame = (result, winningPositions) => {
-    setTurn(null)
+    setTurn(null);
     if (result !== null) {
       setScore({
         ...score,
-        [result] : score[result] + 1
-      })
+        [result]: score[result] + 1,
+      });
+      confetti();
     }
-    setWinningSquares(winningPositions)
+    setWinningSquares(winningPositions);
+
     setTimeout(() => {
-      reset()
-    }, 2000);
-  }
+      reset();
+    }, 3000);
+  };
 
   const reset = () => {
     setTurn('X');
     setSquares(Array(9).fill(null));
     setWinningSquares([]);
-  }
-    
+  };
+  const hardReset = () => {
+    setTurn('X');
+    setSquares(Array(9).fill(null));
+    setWinningSquares([]);
+    setScore({
+      X: 0,
+      O: 0,
+    });
+  };
 
   const handleClick = (value) => {
     let newArray = [...squares];
-    newArray.splice(value, 1 , turn);
+    newArray.splice(value, 1, turn);
     setSquares(newArray);
-    checkForWinner(newArray)
-  }
- 
-
+    checkForWinner(newArray);
+  };
 
   return (
     <div className="container">
-      <Turn
-      turn = {turn}
+      <h1>TIC TAC TOE</h1>
+      <Turn turn={turn} />
+      <Board
+        onClick={handleClick}
+        squares={squares}
+        turn={turn}
+        winningSquares={winningSquares}
       />
-     <Board
-      onClick = {handleClick}
-      squares = {squares}
-      turn={turn}
-      winningSquares = {winningSquares}
-     />
-     <ScoreBoard
-     score = {score}
-     />
+      <ScoreBoard score={score} />
+      <button className="button" onClick={hardReset}>
+        Restart Game
+      </button>
     </div>
   );
-}
+};
 
 export default App;
